a9415a2384b953a32dae7a3b1719512024315193
poncho <161401877+poncho64@users.noreply.github.com>
2025-07-22 13:32:30 -0500

    Subida inicial del servidor de chat

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+
+app = FastAPI()
+clients = []
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    clients.append(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            for client in clients:
+                if client != websocket:
+                    await client.send_text(data)
+    except WebSocketDisconnect:
+        clients.remove(websocket)
